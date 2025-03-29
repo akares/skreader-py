aa27cf745c47b32eea7831a2ea349a2001d62c46
Aleksandr Reshetniak <res.box@gmail.com>
2025-03-29 15:50:49 +0100

    Enable coverage by default

--- scripts/coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running tests with coverage..."
-python -m pytest --cov=skreader --cov-report=term --cov-report=html
-
-echo -e "\nCoverage report generated in htmlcov/index.html"
